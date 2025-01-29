b928784485593ffc173428703f384bcdf98760a0
MrAstatine <akshatkrish2005@gmail.com>
2025-01-29 23:32:22 +0530

    learn query param

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,6 +12,16 @@ def index():
 @app.get("/about")
 def about():
     return {"data": "about page"}
+
+
+@app.get("/blog")
+def some_blog(limit=6, published: bool = False, sort: Optional[str] = None):
+    # published u get here is string. so typecast into boolean for if else statement to work
+    # only gets 10 published blogs
+    if published:
+        return {"data": f"{limit} published blogs from the db list"}
+    else:
+        return {"data": f"{limit} unpublished blogs from the db list"}
 
 
 # if u keep this after /blog/{id} route then here unpublished will b consisderd as a parameter. so just put it up to as it employs dynamic routing
