93670205d8298badc6d7087bcbddff1a6e1e6a12
MrAstatine <akshatkrish2005@gmail.com>
2025-02-14 20:57:33 +0530

    CRUD basic done

--- blog/main.py
+++ blog/main.py
@@ -57,3 +57,32 @@ def show(id, response: Response, db: Session = Depends(get_db)):
             detail=f"Blog with given ID {id} does not exist",
         )
     return blog
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id { id} does not exist",
+        )
+    blog.delete(
+        synchronize_session=False
+    )  # synchronize_session means it will sync the session with the db. we put it as False because we don't want to sync it
+
+    db.commit()  # whenever u do something with db u need to commit it
+    return {"U have been wiped"}
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with ID {id} does not exist",
+        )
+    blog.update({"title": request.title, "body": request.body})
+    db.commit()
+    return {"message": "updated"}
